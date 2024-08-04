bb21ca80de4fe1b340ba2dbf2c0bfc616febc88b
OgAeons <opaeons@gmail.com>
2024-08-04 11:34:19 +0530

    Added basic code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,25 @@
 import React from "react"
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/material.css'
+import 'codemirror/mode/xml/xml'
+import 'codemirror/mode/css/css'
+import 'codemirror/mode/javascript/javascript'
+import Controlled from 'react-codemirror2'
 
 function App() {
     return (
-        <h1> Hello </h1>
+        <div>
+            <div className="editor-section">
+                <div className="editor"></div>
+            </div>
+            <div className="display">
+                <iframe 
+                    title="output"
+                    sandbox="allow-scripts"
+                    style={{border: 0, height: "100%", width: "100%"}}
+                ></iframe>
+            </div>
+        </div>
     )
 }
 
