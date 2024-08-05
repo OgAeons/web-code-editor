41fcf7320cd2d3b15bd88cfa3774a5d8343e8e35
OgAeons <opaeons@gmail.com>
2024-08-05 11:07:41 +0530

    Added render functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,25 @@
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
 import Editor from "./Editor"
 
 function App() {
     const [html, setHtml] = useState("")
     const [css, setCss] = useState("")
     const [javascript, setJavascript] = useState("")
+    const [render, setRender] = useState("")
+
+    function handleCompile() {
+        setRender(`
+            <html>
+                <body>${html}</body>
+                <style>${css}</style>
+                <script>${javascript}</script>
+            </html>
+        `)
+    }
 
     return (
         <div>
+            <button onClick={handleCompile}>Compile</button>
             <div className="editor-section">
                 <Editor 
                     title="HTML"
@@ -30,6 +42,7 @@ function App() {
             </div>
             <div className="display">
                 <iframe 
+                    srcDoc = {render}
                     title="output"
                     sandbox="allow-scripts"
                     style={{border: 0, height: "92%", width: "98%"}}
