f987ffb6d59709688e48430f86561fcb18e50045
OgAeons <opaeons@gmail.com>
2024-08-05 11:41:22 +0530

    Added navbar and autocompile & compile button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,7 @@ function App() {
     const [css, setCss] = useState("")
     const [javascript, setJavascript] = useState("")
     const [render, setRender] = useState("")
+    const [autoCompile, setAutoCompile] = useState(false)
 
     function handleCompile() {
         setRender(`
@@ -17,9 +18,32 @@ function App() {
         `)
     }
 
+    useEffect(() => {
+        if (autoCompile) {
+            const timeout = setTimeout(() => {
+                handleCompile()
+            }, 250)
+            return () => clearTimeout(timeout)
+        }
+    }, [html, css, javascript, autoCompile])
+
+    function toggleAutoCompile() {
+        setAutoCompile(prev => !prev);
+    }
+
     return (
         <div>
-            <button onClick={handleCompile}>Compile</button>
+            <div className="navbar">
+                <h2>Web Code Editor</h2>
+                <p>Auto Compile</p> 
+                <label class="switch">
+                    <input type="checkbox" checked={autoCompile} onChange={toggleAutoCompile}/>
+                    <span class="slider round"></span>
+                </label>
+                <p> | </p>
+                <button onClick={handleCompile}>Compile</button>
+            </div>
+            
             <div className="editor-section">
                 <Editor 
                     title="HTML"
